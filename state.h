2f88c36bc443e21d1a9cd213965bf6b0d01457f3
Jonathan Steinert <hachi@kuiki.net>
2018-01-26 22:40:22 -0800

    Switch all status items to longs

--- state.h
+++ state.h
@@ -6,12 +6,12 @@ extern "C" {
 #endif
 
 struct doorbell_state {
-  volatile unsigned int auto_pulse = 0;
+  volatile long auto_pulse = 0;
 
-  volatile unsigned long ring_at = 0;
-  volatile unsigned long pulse_until = 0;
+  volatile long ring_at = 0;
+  volatile long pulse_until = 0;
 
-  volatile int standing_brightness = 0;
+  volatile long standing_brightness = 0;
 
   bool net_online = false;
   bool snmp_online = false;
