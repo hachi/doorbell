2d90e8f7fbc73e5855390d9643701e8d350192d2
Jonathan Steinert <hachi@kuiki.net>
2018-01-27 20:35:01 -0800

    Add Linux assistance scripts

---
+++ build.sh
@@ -0,0 +1,1 @@
+/opt/arduino/hardware/tools/avr/bin/avr-objcopy -I ihex doorbell.ino.arduino_standard.hex -O binary doorbell.ino.bin
---
+++ put.sh
@@ -0,0 +1,1 @@
+tftp -m binary -v 10.8.0.175 -c put doorbell.ino.bin
